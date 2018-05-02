caf590d56aca5c1fb444f8ebd0af31fbfe5be9f7
Leo Liang <leoliang@gmail.com>
2018-05-02 21:52:51 +0800

    Generate heat map of posts per hour

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ const _ = require('lodash/fp');
 const Database = require('better-sqlite3');
 const echarts = require('node-echarts');
 const moment = require('moment');
+const jieba = require("nodejieba");
 
 const ROOM = '6624467079@chatroom';
 
 function parseMsg(m) {
+  if (!m.content) {
+    return {
+      user: '',
+      content: '',
+      date: moment(m.createTime)
+    }
+  }
   const splitPos = m.content.indexOf(':');
   const user = m.content.substr(0, splitPos);
   const content = m.content.substr(splitPos + 1).trim();
@@ -26,20 +34,16 @@ class BufferReader {
   read(size) {
     if (size === 1) {
       const byte = this.buffer.readInt8(this.offset);
-      // console.log('Read byte at pos', this.offset, byte.toString(16));
       this.offset++;
       return byte;
     } else {
       const s = this.buffer.toString('utf-8', this.offset, this.offset + size);
-      // console.log('Read', size, 'bytes string', s);
-      // console.log(this.buffer.toString('hex', this.offset, this.offset + size));
       this.offset += size;
       return s;
     }
   }
 
   readAdvanceByte(adv) {
-    // console.log('Read advanced at pos', this.offset + adv);
     return this.buffer.readInt8(this.offset + adv);
   }
 }
@@ -101,12 +105,12 @@ function topTalkers(topN, periodFn) {
   );
 }
 
-function countMsgPerTalker(periodFn) {
+function countMsgPerTalker(talkers, periodFn) {
   return _.flow(
+    _.filter(msg => _.includes(msg.user, talkers)),
     _.groupBy('user'),
     _.mapValues(_.flow(
-      _.groupBy(msg => periodFn(msg.date)),
-      _.mapValues('length'),
+      _.countBy(msg => periodFn(msg.date)),
       _.toPairs,
     )),
     _.toPairs,
@@ -114,9 +118,93 @@ function countMsgPerTalker(periodFn) {
   );
 }
 
-function print(obj) {
+function wordList(periodFn) {
+  return _.flow(
+    _.groupBy(msg => periodFn(msg.date)),
+    _.mapValues(_.flow(
+      _.flatMap(msg => jieba.extract(msg.content, 10)),
+      _.map('word'),
+      // _.each(print),
+      _.countBy(_.identity),
+      _.toPairs,
+      _.filter(([word, number]) => word.length > 1),
+      _.sortBy(_.flow(_.tail, parseInt)),
+      _.reverse,
+      _.take(20),
+    )),
+  );
+}
+
+
+function userHeatmap(talkers) {
+
+  function maxValue(data) {
+    return _.flow(
+      _.map(_.last),
+      _.max,
+    )(data);
+  }
+
+  function chart([user, data]) {
+    echarts({
+      width: 500,
+      height: 400,
+      path: `./output/${usernames[user].substr(0, 3)}.png`,
+      option: {
+        title: {
+          text: usernames[user]
+        },
+        xAxis: {
+          type: 'category',
+          data: ['12a', '1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a', '10a', '11a',
+            '12p', '1p', '2p', '3p', '4p', '5p', '6p', '7p', '8p', '9p', '10p', '11p'],
+          splitArea: {
+            interval: 0,
+            show: true
+          }
+        },
+        yAxis: {
+          type: 'category',
+          data: ['Saturday', 'Friday', 'Thursday', 'Wednesday', 'Tuesday', 'Monday', 'Sunday'],
+          splitArea: {
+            interval: 0,
+            show: true
+          }
+        },
+        visualMap: {
+          show: false,
+          min: 0,
+          max: 50, //maxValue(data),
+          color: ['orangered','yellow','lightskyblue'],
+        },
+        series: [{
+          type: 'heatmap',
+          data: data,
+          label: {
+            normal: {
+              show: true
+            }
+          },
+        }]
+      }
+    }).catch(console.log);
+  }
+
+  const period = (moment) => `${moment.hour()}-${moment.day()}`;
+  return _.flow(
+    countMsgPerTalker(talkers, period),
+    _.fromPairs,
+    _.mapValues(
+      _.map(([idx, n]) => _.flatten([idx.split('-').map(v => Number(v)), n]))
+    ),
+    _.toPairs,
+    _.each(chart)
+  );
+}
+
+function print(...obj) {
   console.log('-------------------');
-  console.log(obj);
+  console.log(...obj);
 }
 
 // ========== main ============
@@ -133,12 +221,20 @@ const nicknameMap = _.fromPairs(parseChatroomNicknames(roomdata));
 const usernames = _.defaults(userMap, nicknameMap);
 
 const messages = _.map(parseMsg)(
-  db.prepare('SELECT content, createTime FROM message WHERE talker=? AND type IN (1,49) AND createTime<1524960000000 LIMIT 1000000').all(ROOM)
+  db.prepare('SELECT content, createTime, type FROM message WHERE talker=? AND type IN (1,3,49) AND createTime<1524960000000 LIMIT 10000000').all(ROOM)
 );
 
 const period = (moment) => {
   return moment.month() * 3 + parseInt(moment.date() / 10);
 };
+
+const periodQuarter = (moment) => {
+  return moment.quarter();
+};
+
+// const words = wordList(period)(messages);
+// print(words);
+// process.exit();
 
 const talkers = _.flow(
   _.mapValues(
@@ -148,12 +244,10 @@ const talkers = _.flow(
   _.map(_.tail),
   _.flattenDeep,
   _.uniq
-)(topTalkers(2, period)(messages));
+)(topTalkers(5, period)(messages));
 
-const out = _.flow(
-  _.filter(msg => _.includes(msg.user, talkers)),
-  countMsgPerTalker(period)
-)(messages);
+
+const out = countMsgPerTalker(talkers, period)(messages);
 
 // print(out);
 // console.log(JSON.stringify(out));
@@ -161,11 +255,11 @@ const out = _.flow(
 echarts({
   width: 800,
   height: 1000,
-  path: './output/out.png',
+  path: './output/toptalker.png',
   option: {
     backgroundColor: '#fff',
-    color: ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9', 
-    '#f15c80', '#e4d354', '#8085e8', '#8d4653', '#91e8e1'],
+    color: ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9',
+      '#f15c80', '#e4d354', '#8085e8', '#8d4653', '#91e8e1'],
     xAxis: {
       type: 'value',
       interval: 1,
@@ -194,4 +288,10 @@ echarts({
       )(out)
     }
   }
-}).then(() => process.exit(0)).catch(console.log);
+})
+  // .then(() => process.exit(0))
+  .catch(console.log);
+
+
+
+userHeatmap(talkers)(messages);
